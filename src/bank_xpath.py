e30d1882d3cfb299024daa8ca279b7a811ac8552
hvsio <h4sio@o2.pl>
2019-11-12 12:01:31 +0100

    exchange100

--- src/bank_xpath.py
+++ src/bank_xpath.py
@@ -41,7 +41,7 @@ class BankXpath:
             errors.append(ERRORS["empty_url"])
         if not validators.url(self.pageurl):
             errors.append(ERRORS["bank_url_error"])
-        if not self.unit == "M100" and not self.unit == "M1000" and not self.unit == "exchange" \
+        if not self.unit == "M100" and not self.unit == "exchange100" and not self.unit == "exchange" \
                 and not self.unit == "percentage":
             errors.append(ERRORS["wrong_unit"])
         try:
