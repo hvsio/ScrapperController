e923da7b8c9f26f3cb486825494450d203a2822c
FancyNameCreator <szymonso1009@gmail.com>
2019-11-18 21:19:32 +0100

    update to handle fees - all 4 requests are working. May need some polishing :)

--- src/controller.py
+++ src/controller.py
@@ -1,6 +1,9 @@
+import json
+
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from src.bank_xpath import BankXpath
+from src.fees import Fee
 from src.mongo_connection import MongoConnection
 
 app = Flask(__name__)
@@ -15,6 +18,7 @@ def on_get():
 @app.route('/banks', methods=['POST'])
 def on_post():
     posted_data = request.get_json()
+    print(posted_data)
     bank = BankXpath(**posted_data)
     validation = bank.validate()
     if validation:
@@ -37,6 +41,41 @@ def on_put():
 @app.route('/banks/<string:bank_id>', methods=['DELETE'])
 def on_delete(bank_id):
     return MongoConnection.delete(bank_id)
+
+
+# ----------- Szymon's efforts below :) ------
+
+@app.route('/fees/get', methods=['GET'])
+def on_get_fees():
+    return MongoConnection.get_fees()
+
+
+@app.route('/fees/post', methods=['POST'])
+def on_post_fees():
+    posted_data = request.get_json()
+    print(posted_data)
+    fee = Fee(**posted_data)
+    validation = fee.validate()
+    if validation:
+        return validation
+    else:
+        return MongoConnection.add_fee(fee)
+
+
+@app.route('/fees/put', methods=['PUT'])
+def on_put_fees():
+    posted_data = request.get_json()
+    fee = Fee(**posted_data)
+    validation = fee.validate()
+    if validation:
+        return validation
+    else:
+        return MongoConnection.update_fee(fee)
+
+
+@app.route('/fees/<string:fee_id>/delete', methods=['DELETE'])
+def on_delete_fees(fee_id):
+    return MongoConnection.delete_fee(fee_id)
 
 
 if __name__ == '__main__':
--- src/mongo_connection.py
+++ src/mongo_connection.py
@@ -95,3 +95,79 @@ class MongoConnection:
         # TODO log exception variable
         print(str(exception))
         return MongoConnection.TimeoutResponse
+
+
+    # ----------- Szymon's efforts below :) ------
+
+    @staticmethod
+    def update_fee(fee):
+        try:
+            databaseRef = MongoConnection.connect_to_fees_database()
+            query = {"id": fee.id}
+            data = fee.to_JSON()
+            if databaseRef.find_one(query):
+                databaseRef.find_one_and_replace(query, data)
+                return Response(response=json.dumps({'status': 'fee updated'}),
+                                status=200,
+                                mimetype='application/json')
+            else:
+                return MongoConnection.add_bank(fee)
+        except Exception as e:
+            return MongoConnection.log_and_return_error_response(e)
+
+    @staticmethod
+    def delete_fee(fee_id):
+        try:
+            databaseRef = MongoConnection.connect_to_fees_database()
+            query = {"id": fee_id}
+            result = databaseRef.find_one(query)
+            if result:
+                databaseRef.delete_one(query)
+                return Response(response=json.dumps({'status': 'deleted'}),
+                                status=200,
+                                mimetype='application/json')
+            else:
+                return Response(response=json.dumps({'status': 'fee does not exist'}),
+                                status=400,
+                                mimetype='application/json')
+        except Exception as e:
+            return MongoConnection.log_and_return_error_response(e)
+
+    @staticmethod
+    def connect_to_fees_database():
+        Config.initialize()
+        environment = Config.cloud('DATABASE') if (len(sys.argv) > 1 and sys.argv[1] == 'cloud') else Config.dev(
+            'DATABASE')
+        myClient = pymongo.MongoClient(environment)
+        fees_db = myClient["Fees"]
+        return fees_db["paymentfees"]
+
+    @staticmethod
+    def add_fee(fee):
+        try:
+            databaseRef = MongoConnection.connect_to_fees_database()
+            data = fee.to_JSON()
+            query = {"country": fee.country}
+            if databaseRef.find_one(query):
+                return Response(response=json.dumps({'status': 'country already exists'}),
+                                status=400,
+                                mimetype='application/json')
+            else:
+                databaseRef.insert(data)
+                return Response(response=json.dumps({'status': 'added'}),
+                                status=201,
+                                mimetype='application/json')
+        except Exception as e:
+            return MongoConnection.log_and_return_error_response(e)
+
+    @staticmethod
+    def get_fees():
+        try:
+            data = MongoConnection.connect_to_fees_database().find({}, {"_id": 0, })
+            fees = dumps(data, sort_keys=True, indent=4, separators=(',', ': '))
+            return Response(response=fees,
+                            status=201,
+                            mimetype='application/json')
+        except Exception as e:
+            return MongoConnection.log_and_return_error_response(e)
+
